dd7d83595ae58a0dc250b6d44e9a9453d11d46f4
Stefano <stefa99graziani@gmail.com>
2020-06-07 19:11:54 -0300

    test funcionando de como usar map para objetos

--- TEST.cpp
+++ TEST.cpp
@@ -1,19 +1,55 @@
 #include <iostream>
-#include <set>
+#include <map>
 
-int main () {
-   std::set<int> myset;
-   std::set<int>::iterator it;
+using namespace std;
 
-   for (int i = 1; i <= 10; i++) myset.insert(i*10);
-   it = myset.find(40);
-   myset.erase (it);
-   myset.erase (myset.find(60));
+class Asignatura{
+  private:
+    int codigo;
+    string nombre;
+    bool teorico;
+    bool monitoreo;
+    bool practico;
+  public:
+    Asignatura(){};
 
-   std::cout << "myset contains:";
-   for (it = myset.begin(); it!=myset.end(); ++it)
-      std::cout << ' ' << *it;
-   std::cout << '\n';
+    void setCodigo(int c){ this->codigo = c;};
+    int getCodigo() const{ return codigo;};
+
+    void setNombre(string n){ this->nombre = n;};
+    string getNombre()const{ return nombre;};
+
+    void setTeorico(bool t){  this->teorico = t;};
+    bool getTeorico() const{ return teorico;};
+
+    void setPractico(bool p){ this->practico = p;};
+    bool getPractico() const{ return practico;};
+
+    void setMonitoreo(bool m){ this->monitoreo = m;};
+    bool getMonitoreo() const{  return monitoreo;};
+
+    ~Asignatura(){};
+
+/*    bool operator< (const Asignatura &right) const{
+    return codigo < right.codigo;
+  }*/
+};
+
+int main(void) {
+   Asignatura *asig = new Asignatura();
+   asig->setCodigo(123);
+   asig->setNombre("Calculo");
+   asig->setTeorico(true);
+   asig->setPractico(true);
+   asig->setMonitoreo(false);
+   map<int, Asignatura> m = {
+            {asig->getCodigo(), *asig},
+            };
+
+   auto it = m.find(asig->getCodigo());
+
+   cout << "Iterator points to " << it->first <<
+      " = " << it->second.getCodigo() << endl;
 
    return 0;
 }
